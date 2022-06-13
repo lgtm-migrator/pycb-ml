d1c61455a609301776ba95ece08fb6397b563ab5
Martin Miglio <marmig0404@gmail.com>
2022-06-12 21:50:35 -0400

    print out statistics about photos in training dirs
    Former-commit-id: 04902c1f928cdbae181c6142cbffab5481cc8fd1

---
+++ data_statistics.py
@@ -0,0 +1,21 @@
+import os
+
+module_path = os.path.dirname(os.path.realpath(__file__))
+train_data_dir = os.path.join(module_path, 'data', 'train')
+validation_data_dir = os.path.join(module_path, 'data', 'validate')
+
+def get_class_stats(classes, data_dir) -> dict[str,int]:
+    stats = dict()
+    for class_name in classes:
+        class_data_dir = os.path.join(data_dir, class_name)
+
+        file_names = [os.path.join(class_data_dir, file_name) for file_name in next(
+            os.walk(class_data_dir), (None, None, []))[2]]
+        stats[class_name] = len(file_names)
+    return stats
+
+classes = ["A1", "A3", "B1", "B3", "C1", "C3", "None"]
+class_stats = get_class_stats(classes, train_data_dir)
+
+for key in class_stats:
+    print(f"{key} : {class_stats[key]}")
--- train_model.py
+++ train_model.py
@@ -1,27 +1,81 @@
 import os
+import random
 import time
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+import tempfile
+import shutil
 
-module_path = os.path.dirname(os.path.realpath(__file__))
-train_data_dir = os.path.join(module_path, 'data', 'train')
-validation_data_dir = os.path.join(module_path, 'data', 'validate')
-model_path = os.path.join(
-    module_path,
-    "models",
-    str(int(time.time())),
-    "model_saved.m5"
-)
+from data_statistics import get_class_stats
 
-img_width, img_height = 224, 224
 
-nb_train_samples = 400
-nb_validation_samples = 100
-epochs = 10
-batch_size = 16
+def copy_list_of_files(file_list, desitation_dir):
+    for file_name in file_list:
+        shutil.copy2(file_name, desitation_dir)
+
+
+def copy_files_to_temp(classes, train_data_dir):
+    temp_train_dir = tempfile.mkdtemp()
+    temp_validate_dir = tempfile.mkdtemp()
+    # normalize training data count
+    print("Input data stats:")
+    class_stats = get_class_stats(classes, train_data_dir)
+    smallest_class_size = min(class_stats.values())
+
+    for class_name in classes:
+        class_data_dir = os.path.join(train_data_dir, class_name)
+
+        # make dirs for class in temp dir
+        os.makedirs(os.path.join(temp_train_dir, class_name))
+        os.makedirs(os.path.join(temp_validate_dir, class_name))
+
+        # get file list for class cut to smallest class size
+        file_names = [os.path.join(class_data_dir, file_name) for file_name in next(
+            os.walk(class_data_dir), (None, None, []))[2]][:smallest_class_size]
+
+        # calculate sample sizes
+        validation_proportion = 0.2
+        validate_sample_size = int(
+            len(file_names) * validation_proportion)
+
+        # create train and validate samples
+        validate_sample = random.sample(
+            file_names, validate_sample_size)
+        train_sample = [
+            x for x in file_names if x not in validate_sample]
+
+        # copy samples into directories
+        copy_list_of_files(validate_sample, os.path.join(
+            temp_validate_dir, class_name))
+        copy_list_of_files(train_sample, os.path.join(
+            temp_train_dir, class_name))
+
+    print("Normalized training data stats:")
+    class_stats = get_class_stats(classes, temp_train_dir)
+    print("Normalized validation data stats:")
+    class_stats = get_class_stats(classes, temp_validate_dir)
+
+    return temp_train_dir, temp_validate_dir
+
+
+def prep_files_for_training(classes):
+    module_path = os.path.dirname(os.path.realpath(__file__))
+    train_data_dir = os.path.join(module_path, 'data', 'train')
+
+    model_path = os.path.join(
+        module_path,
+        "models",
+        str(int(time.time())),
+        "model_saved.m5"
+    )
+
+    temp_train_dir, temp_validate_dir = copy_files_to_temp(
+        classes, train_data_dir)
+
+    return temp_train_dir, temp_validate_dir, model_path
 
 
 def compile_model(img_width, img_height):
@@ -45,11 +99,11 @@ def compile_model(img_width, img_height):
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())
-    model.add(Dense(64))
+    model.add(Dense(128))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1))
-    model.add(Activation('sigmoid'))
+    model.add(Activation('softmax'))
 
     model.compile(loss='binary_crossentropy',
                   optimizer='rmsprop',
@@ -90,7 +144,25 @@ def train_model(train_data_dir, validation_data_dir, img_width, img_height, nb_t
     model.save_weights(model_path)
 
 
+img_width, img_height = 290, 325
+classes = ["A1", "A3", "B1", "B3", "C1", "C3", "None"]
+
+
+train_data_dir, validation_data_dir, model_path = prep_files_for_training(
+    classes)
+
+nb_train_samples = sum(len(files) for _, _, files in os.walk(
+    os.path.join(train_data_dir, classes[0])))
+nb_validation_samples = sum(len(files) for _, _, files in os.walk(
+    os.path.join(validation_data_dir, classes[0])))
+
+epochs = 50
+batch_size = 15
+
 model = compile_model(img_width, img_height)
 
 train_model(train_data_dir, validation_data_dir, img_width, img_height,
             nb_train_samples, nb_validation_samples, epochs, batch_size, model, model_path)
+
+shutil.rmtree(train_data_dir)
+shutil.rmtree(validation_data_dir)
