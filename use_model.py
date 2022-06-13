a68e4d8231b7554de15ded4daa9bfb2b2a85ea39
Martin Miglio <marmig0404@gmail.com>
2022-06-13 13:48:42 -0400

    model testing

--- use_model.py
+++ use_model.py
@@ -7,28 +7,30 @@ import numpy as np
 
 from keras.models import load_model
 
-model_name = "1655102616"
-validation_image_name = "test/1.png"
+model_name = "save1655109194"
+test_image_name = "1654894163.png"
+img_width, img_height = (290, 325)
 
 module_path = os.path.dirname(os.path.realpath(__file__))
 model_path = os.path.join(
     module_path,
     "models",
-    model_name,
-    "model_saved.m5"
+    model_name
 )
 
 model = load_model(model_path)
 
 
-test_data_dir = os.path.join(module_path, 'data', 'test')
-test_image = os.path.join(test_data_dir, validation_image_name)
+test_data_dir = os.path.join(module_path, 'data', 'train', 'B1')
+test_image = os.path.join(test_data_dir, test_image_name)
 
-img_width, img_height = (355, 515)
+
 
 image = load_img(test_image, target_size=(img_width, img_height))
 img = np.array(image)
 img = img / 255.0
 img = img.reshape(1, img_width, img_height, 3)
 label = model.predict(img)
-print("Predicted Class: ", label[0][0])
+round_label = np.around(label)
+
+print("Predicted Class: ", round_label)
