c13ce87ecd5c14f0179f8085d8b5f2dcb37034b2
Martin Miglio <marmig0404@gmail.com>
2022-06-13 17:49:36 -0400

    switch to plotly over matplotlib extract methods to other files create main

--- train_model.py
+++ train_model.py
@@ -1,10 +1,6 @@
 import os
-import random
-import shutil
-import tempfile
-import time
 
-import matplotlib.pyplot as plt
+import plotly.express as px
 from keras import backend as K
 from keras.applications.vgg16 import VGG16
 from keras.layers import Dense, Flatten
@@ -12,80 +8,13 @@ from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import set_random_seed
 from numpy.random import seed
+from pandas import DataFrame
 
-from data_statistics import get_class_stats, print_statistics
+from class_statistics import get_class_stats, save_stats_to_file
+from file_utils import clean_up_after_training, prep_files_for_training
 
-set_random_seed(2)
-seed(1)
-
-
-def copy_list_of_files(file_list, desitation_dir):
-    for file_name in file_list:
-        shutil.copy2(file_name, desitation_dir)
-
-
-def copy_files_to_temp(classes, train_data_dir):
-    temp_train_dir = tempfile.mkdtemp()
-    temp_validate_dir = tempfile.mkdtemp()
-
-    class_stats = get_class_stats(classes, train_data_dir)
-    print("Input data stats:")
-    print_statistics(class_stats)
-
-    # get value to normalize training data count
-    smallest_class_size = min(class_stats.values())
-
-    for class_name in classes:
-        class_data_dir = os.path.join(train_data_dir, class_name)
-
-        # make dir for class in temp dirs
-        os.makedirs(os.path.join(temp_train_dir, class_name))
-        os.makedirs(os.path.join(temp_validate_dir, class_name))
-
-        # get file list for class cut to smallest class size
-        file_names = [os.path.join(class_data_dir, file_name) for file_name in next(
-            os.walk(class_data_dir), (None, None, []))[2]]  # [:smallest_class_size]
-
-        # calculate sample sizes
-        validation_proportion = 0.2
-        validate_sample_size = int(
-            len(file_names) * validation_proportion)
-
-        # create train and validate samples
-        validate_sample = random.sample(
-            file_names, validate_sample_size)
-        train_sample = [
-            x for x in file_names if x not in validate_sample]
-
-        # copy samples into directories
-        copy_list_of_files(validate_sample, os.path.join(
-            temp_validate_dir, class_name))
-        copy_list_of_files(train_sample, os.path.join(
-            temp_train_dir, class_name))
-
-    print("Normalized training data stats:")
-    print_statistics(get_class_stats(classes, temp_train_dir))
-    print("Normalized validation data stats:")
-    print_statistics(get_class_stats(classes, temp_validate_dir))
-
-    return temp_train_dir, temp_validate_dir
-
-
-def prep_files_for_training(classes):
-    module_path = os.path.dirname(os.path.realpath(__file__))
-
-    train_data_dir = os.path.join(module_path, 'data', 'train')
-
-    model_save_path = os.path.join(
-        module_path,
-        "models",
-        f"save{str(int(time.time()))}"
-    )
-
-    temp_train_dir, temp_validate_dir = copy_files_to_temp(
-        classes, train_data_dir)
-
-    return temp_train_dir, temp_validate_dir, model_save_path
+set_random_seed(8)
+seed(8)
 
 
 def compile_model(img_width, img_height):
@@ -107,8 +36,7 @@ def compile_model(img_width, img_height):
     return model
 
 
-
-def train_model(train_data_dir, validation_data_dir, img_width, img_height, epochs, batch_size, model: Model, model_save_path):
+def fit_model(train_data_dir, validation_data_dir, img_width, img_height, epochs, batch_size, model: Model, model_save_path):
     # reference https://www.geeksforgeeks.org/python-image-classification-using-keras/
     train_datagen = ImageDataGenerator()
     validation_datagen = ImageDataGenerator()
@@ -142,44 +70,76 @@ def train_model(train_data_dir, validation_data_dir, img_width, img_height, epoc
     return history
 
 
-def plot_history(history, model_save_path):
-    # summarize history for accuracy
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig(os.path.join(model_save_path, "model_accuracy.png"))
-# summarize history for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig(os.path.join(model_save_path, "model_loss.png"))
+def save_model_history(history, model_save_path, train_class_stats, validation_class_stats):
+    # save stats to file
+    save_stats_to_file(
+        train_class_stats,
+        os.path.join(model_save_path, "train_class_stats.txt")
+    )
+    save_stats_to_file(
+        validation_class_stats,
+        os.path.join(model_save_path, "validation_class_stats.txt")
+    )
+
+    # save accuracy and loss charts to file
+    accuracy_history = DataFrame({
+        'epoch': history.epoch,
+        'accuracy': history.history['accuracy'],
+        'val_accuracy': history.history['val_accuracy']
+    })
+    fig = px.line(
+        accuracy_history,
+        x="epoch",
+        y=[accuracy_history['accuracy'], accuracy_history['val_accuracy']],
+        title=f"Accuracy history - {os.path.basename(model_save_path)}"
+    )
+    fig.write_html(os.path.join(model_save_path, "model_accuracy.html"))
+
+    loss_history = DataFrame({
+        'epoch': history.epoch,
+        'loss': history.history['loss'],
+        'val_loss': history.history['val_loss']
+    })
+    fig = px.line(
+        loss_history,
+        x="epoch",
+        y=[loss_history['loss'], loss_history['val_loss']],
+        title=f"Loss history - {os.path.basename(model_save_path)}"
+    )
+    fig.write_html(os.path.join(model_save_path, "model_loss.html"))
 
 
-img_width, img_height = 290, 325
-classes = ["A1", "A3", "B1", "B3", "C1", "C3", "None"]
+def main(img_width, img_height, classes, epochs, batch_size):
+    # collect stats on and prepare traiing and validation files
+    train_data_dir, validation_data_dir, model_save_path = prep_files_for_training(
+        classes)
+
+    train_class_stats = get_class_stats(classes, train_data_dir)
+    validation_class_stats = get_class_stats(classes, validation_data_dir)
+
+    train_size = train_class_stats[classes[0]]
+    validation_size = validation_class_stats[classes[0]]
+
+    # create and train model, save training info
+    model = compile_model(img_width, img_height)
+
+    history = fit_model(train_data_dir, validation_data_dir, img_width,
+                        img_height, epochs, batch_size, model, model_save_path)
+
+    save_model_history(history, model_save_path,
+                       train_class_stats, validation_class_stats)
+
+    # clean up
+    clean_up_after_training(train_data_dir, validation_data_dir)
 
 
-train_data_dir, validation_data_dir, model_save_path = prep_files_for_training(
-    classes)
+if __name__ == "__main__":
+    # image and class params
+    img_width, img_height = 290, 325
+    classes = ["A1", "A3", "B1", "B3", "C1", "C3", "None"]
 
-train_size = get_class_stats(classes, train_data_dir)[classes[0]]
-validation_size = get_class_stats(classes, validation_data_dir)[classes[0]]
+    # training params
+    epochs = 2
+    batch_size = 5
 
-epochs = 200
-batch_size = 16
-
-model = compile_model(img_width, img_height)
-
-history = train_model(train_data_dir, validation_data_dir, img_width,
-                      img_height, epochs, batch_size, model, model_save_path)
-
-plot_history(history, model_save_path)
-
-shutil.rmtree(train_data_dir)
-shutil.rmtree(validation_data_dir)
+    main(img_width, img_height, classes, epochs, batch_size)
