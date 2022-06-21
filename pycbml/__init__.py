e7e025815d65068a01a5c665f91aaed845b9e4ec
Martin Miglio <marmig0404@gmail.com>
2022-06-21 14:35:23 -0400

    fix init imports

--- pycbml/__init__.py
+++ pycbml/__init__.py
@@ -1,6 +1,6 @@
-from classify.train_model import train_new_model, save_model_history, fit_model, compile_model
-from classify.class_statistics import class_stats, print_statistics
-from classify.test_model import eval_and_test_model
+from .classify.train_model import train_new_model, save_model_history, fit_model, compile_model
+from .classify.class_statistics import class_stats, print_statistics
+from .classify.test_model import eval_and_test_model
 
 
 __all__ = [
