fecd681b014c58611198f76162dee34a2d45e312
Martin Miglio <marmig0404@gmail.com>
2022-06-21 14:47:45 -0400

    fix exports

--- pycbml/__init__.py
+++ pycbml/__init__.py
@@ -1,14 +1,7 @@
-from .classify.train_model import train_new_model, save_model_history, fit_model, compile_model
-from .classify.class_statistics import class_stats, print_statistics
-from .classify.test_model import eval_and_test_model
+from . import classify
+
 
 
 __all__ = [
-    'train_new_model',
-    'save_model_history',
-    'fit_model',
-    'compile_model',
-    'class_stats',
-    'print_statistics',
-    'eval_and_test_model'
+    'classify'
 ]
